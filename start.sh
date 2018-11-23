8acced029c2fe277d64ef44e53c07936deca1241
weilluo <luowei---010101@163.com>
2018-11-23 15:55:31 +0800

    send email test

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # start ss server
-forever start -c "ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:4000" /root/.ssmgr/default.yml
+forever start -c "ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:4000" ~/.ssmgr/default.yml
 
 # start ss-manager
-forever start -c "ssmgr -c" /root/.ssmgr/default.yml
+forever start -c "ssmgr -c" ~/.ssmgr/default.yml
 
 # start webgui
-forever start -c "ssmgr -c" /root/.ssmgr/webgui.yml
+forever start -c "ssmgr -c" ~/.ssmgr/webgui.yml
 
 # Extra line added in the script to run all command line arguments
 exec "$@";
